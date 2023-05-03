bfb1c1585fd3e31d1b45b89765f8128eeb19dc20
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-05-03 08:40:12 +0000

    utils に DesignDataInterface の型付け

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 import * as fs from "fs";
 import dotenv from "dotenv";
+import { DesignDataInterface } from "./DesignData.interface";
 dotenv.config();
 
 export const fetchAllDesignData = async () => {
   const response = await fetch(process.env.PG_DATA_ENDPOINT as string);
-  return (await response.json()) as any;
+  return (await response.json()) as DesignDataInterface;
 };
 
 export const generateFile = (content: string, outputFilePath: string): void => {
