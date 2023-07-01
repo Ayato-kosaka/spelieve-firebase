c95fae8558a9054465492f32ba5ed4ca35387c65
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-07-01 14:16:54 +0000

    Refactoring

--- README.md
+++ README.md
@@ -8,14 +8,18 @@ Spelieve 扱う firebase の以下をソースコードリポジトリ
 
 ※Cloud Functions は取り扱わない
 
-## GitHub Actions 
+## ENV file
+
+https://docs.google.com/document/d/1_wSoCUwnpskPawzY8AQFXzJeRdND9jfBH5ZrwEajKpA/edit
+
+## GitHub Actions
 
 ### Firestore Index
 
 - Pull Firestore Index
 - Deploy Firestore Index
 
-
 ## Reference
+
 - [Firebase CLI reference](https://firebase.google.com/docs/cli/)
 - [firebase-tools](https://github.com/firebase/firebase-tools)
--- src/generateFirestoreRules.ts
+++ src/generateFirestoreRules.ts
@@ -1,104 +1,136 @@
 import { SecurityRules } from "./SecurityRules";
 import { fetchAllDesignData, generateFile, getFirebaseConfig } from "./utils";
 
+// Firestoreのセキュリティルールを生成する非同期関数
 const generateFirestoreRules = async () => {
+  // すべてのデザインデータを取得します
   const allDesignData = await fetchAllDesignData();
 
-  const disallowUpdatesOnRestrictedFields = ({ t_id }: { t_id: string }) => {
-    const columns = allDesignData.T_COLUMNS.filter((c) => c.t_id === t_id);
-    return `!dataDiff().affectedKeys().hasAny([${columns
-      .filter((c) => c.c_updatable === false)
-      .map((c) => `'${c.c_name}'`)
-      .join(", ")}].toSet())`;
-  };
-
-  const recusiveMatchTemplate = ({
+  const recusiveTableRule = ({
     parent_tid,
     parentPath,
   }: {
     parent_tid: string;
     parentPath: string;
   }): string => {
-    const tables = allDesignData.TABLES.filter(
+    const current_tables = allDesignData.TABLES.filter(
       (t) => t.parent_tid === parent_tid
     );
-    if (tables.length === 0) return "";
+    if (current_tables.length === 0) return "";
 
-    const standardMethods = ["get", "list", "create", "update", "delete"];
-    return tables
+    return current_tables
       .map((table) => {
-        const variable = `${table.t_name}ID`;
-        const path = `${parentPath}/${table.t_name}/$(${variable})`;
-        const securityRules = allDesignData.SECURITY_RULES.filter(
-          (sr) => sr.t_id === table.t_id
-        );
-        const allows = standardMethods.reduce((prev, method) => {
-          return {
-            ...prev,
-            [method]: [
-              // @ts-ignore allow_get などのプロパティが存在する
-              table["allow_" + method],
-              ...securityRules
-                .filter((sr) => sr.crud_type === method)
-                .map((sr) =>
-                  SecurityRules[sr.func_id]({
-                    designData: allDesignData,
-                    ...sr,
-                  })
-                ),
-              ...[
-                method === "update"
-                  ? disallowUpdatesOnRestrictedFields({ t_id: table.t_id })
-                  : undefined,
-              ].filter((v) => v !== undefined),
-            ].join(" && "),
-          };
-        }, {} as { [method: string]: string });
-        return `
-    match /${table.t_name}/{${variable}} {
-      
-      function get${table.t_name}Data() {
-        return get(${path}).data
-      }
-      
-      ${Object.entries(allows)
-        .map(([method, allow]) => {
-          return `allow ${method}: if ${allow};`;
-        })
-        .join("\n")}${recusiveMatchTemplate({
-          parent_tid: table.t_id,
-          parentPath: path,
-        })}
-    }`;
+        const tableVariable = `${table.t_name}ID`;
+        const path = `${parentPath}/${table.t_name}/$(${tableVariable})`;
+        // crudConditionObj は get, list, create, update, delete に対してそれぞれの allow_XXX のセキュリティルールを定義する
+        // table に allow_get などのプロパティが存在するので、それを条件に加える
+        // また、SECURITY_RULES に定義が存在する場合はそれを条件に加える
+        const crudConditionObj = {
+          get: [
+            table["allow_get"],
+            ...allDesignData.SECURITY_RULES.filter(
+              (sr) => sr.t_id === table.t_id && sr.crud_type === "get"
+            ).map((sr) =>
+              SecurityRules[sr.func_id]({
+                designData: allDesignData,
+                ...sr,
+              })
+            ),
+          ].join(" && "),
+          list: [
+            table["allow_list"],
+            ...allDesignData.SECURITY_RULES.filter(
+              (sr) => sr.t_id === table.t_id && sr.crud_type === "list"
+            ).map((sr) =>
+              SecurityRules[sr.func_id]({
+                designData: allDesignData,
+                ...sr,
+              })
+            ),
+          ].join(" && "),
+          create: [
+            table["allow_create"],
+            ...allDesignData.SECURITY_RULES.filter(
+              (sr) => sr.t_id === table.t_id && sr.crud_type === "create"
+            ).map((sr) =>
+              SecurityRules[sr.func_id]({
+                designData: allDesignData,
+                ...sr,
+              })
+            ),
+          ].join(" && "),
+          update: [
+            table["allow_update"],
+            ...allDesignData.SECURITY_RULES.filter(
+              (sr) => sr.t_id === table.t_id && sr.crud_type === "update"
+            ).map((sr) =>
+              SecurityRules[sr.func_id]({
+                designData: allDesignData,
+                ...sr,
+              })
+            ),
+            // 制限されたフィールドに対する更新を禁止する
+            `!dataDiff().affectedKeys().hasAny([${allDesignData.T_COLUMNS.filter(
+              (c) => c.t_id === table.t_id && c.c_updatable === false
+            )
+              .map((c) => `'${c.c_name}'`)
+              .join(", ")}].toSet())`,
+          ].join(" && "),
+          delete: [
+            table["allow_delete"],
+            ...allDesignData.SECURITY_RULES.filter(
+              (sr) => sr.t_id === table.t_id && sr.crud_type === "delete"
+            ).map((sr) =>
+              SecurityRules[sr.func_id]({
+                designData: allDesignData,
+                ...sr,
+              })
+            ),
+          ].join(" && "),
+        };
+        return [
+          `match /${table.t_name}/{${tableVariable}} {`,
+          ` function get${table.t_name}Data() {`,
+          `   return get(${path}).data`,
+          ` }`,
+          ` allow get: if ${crudConditionObj.get};`,
+          ` allow list: if ${crudConditionObj.list};`,
+          ` allow create: if ${crudConditionObj.create};`,
+          ` allow update: if ${crudConditionObj.update};`,
+          ` allow delete: if ${crudConditionObj.delete};`,
+          // 子テーブルのセキュリティルールを再帰的に定義する
+          recusiveTableRule({
+            parent_tid: table.t_id,
+            parentPath: path,
+          }),
+          `}`,
+        ].join("\n");
       })
       .join("\n");
   };
 
-  const template = () => {
-    return `rules_version = '2';
-service cloud.firestore {
-  match /databases/{database}/documents {
-    function existingData() {
-      return resource.data;
-    }
+  return `
+    rules_version = '2';
+    service cloud.firestore {
+      match /databases/{database}/documents {
+        function existingData() {
+          return resource.data;
+        }
 
-    function incomingData() {
-      return request.resource.data;
-    }
+        function incomingData() {
+          return request.resource.data;
+        }
 
-    function dataDiff() {
-      return incomingData().diff(existingData());
-    }
+        function dataDiff() {
+          return incomingData().diff(existingData());
+        }
 
-    ${recusiveMatchTemplate({
-      parent_tid: "",
-      parentPath: "/databases/$(database)/documents",
-    })}
-  }
-}`;
-  };
-
-  return template();
+        ${recusiveTableRule({
+          parent_tid: "",
+          parentPath: "/databases/$(database)/documents",
+        })}
+      }
+    }`;
 };
 
 const main = async () => {
